4954c57a8d2077a221c76653c358e6aaadd476c0
Danilo Cavalcanti <danilocavalcanti@aluno.puc-rio.br>
2025-01-20 15:23:51 +0100

    restructuring the regularElement class as a superclass

--- src/element/RegularElement.m
+++ src/element/RegularElement.m
@@ -11,36 +11,33 @@
 classdef RegularElement < handle    
     %% Public attributes
     properties (SetAccess = public, GetAccess = public)
-        type       = 'ISOQ4';       % type of element
-        shape      = [];            % Object of the Shape class
-        node       = [];            % Nodes of the fem mesh
-        connect    = [];            % Nodes connectivity
-        t          = 1.0;           % Thickness
-        mat        = [];            % Vector with material properties
-        intOrder   = 2;             % Order of the numerical integration
-        nnd_el     = 4;             % Number of nodes per element
-        ndof_nd    = 1;             % Number of dof per node
-        glp        = [];            
-        glpg       = [];            % Vector of the regular degrees of freedom
-        gle        = [];            % Vector of the degrees of freedom
-        nglp       = 0;             % Number of regular p-dof
-        ngle       = 0;             % Number of total dof
-        ue         = [];            % Element's displacement vector
-        due        = [];            % Element's increment displacement
-        nIntPoints = 1;             % Number of integration points
-        intPoint   = [];            % Vector with integration point objects
-        result     = [];            % Result object to plot the results
-        isEnriched = false;         % Flag to check if the element is enriched
-        massLumping = false;        % Flag to apply a diagonalization of the compressibility matrix
-        lumpStrategy = 1;
-        isAxisSymmetric = false;
+        type         = 'ISOQ4';       % type of element
+        shape        = [];            % Object of the Shape class
+        node         = [];            % Nodes of the fem mesh
+        connect      = [];            % Nodes connectivity
+        t            = 1.0;           % Thickness
+        mat          = [];            % Vector with material properties
+        intOrder     = 2;             % Order of the numerical integration
+        nnd_el       = 4;             % Number of nodes per element
+        ndof_nd      = 1;             % Number of dof per node
+        gle          = [];            % Vector of the degrees of freedom
+        ngle         = 0;             % Total number of dofs
+        ue           = [];            % Element's displacement vector
+        due          = [];            % Element's increment displacement
+        nIntPoints   = 1;             % Number of integration points
+        intPoint     = [];            % Vector with integration point objects
+        result       = [];            % Result object to plot the results
+        isEnriched   = false;         % Flag to check if the element is enriched
+        massLumping  = false;         % Flag to apply a diagonalization of the compressibility matrix
+        lumpStrategy = 1;             % Id of the diagonalization strategy
+        isAxisSymmetric = false;      % Flag to axissymetric models
     end
     
     %% Constructor method
     methods
         %------------------------------------------------------------------
         function this = RegularElement(type, node, elem, t, ...
-                mat, intOrder, glp, glpg, massLumping, lumpStrategy,isAxisSymmetric)
+                mat, intOrder, massLumping, lumpStrategy,isAxisSymmetric)
             if (nargin > 0)
                 if strcmp(type,'ISOQ4')
                     this.shape = Shape_ISOQ4();
@@ -57,14 +54,6 @@ classdef RegularElement < handle
                 this.t        = t;
                 this.mat      = mat;
                 this.intOrder = intOrder;
-                this.glp      = glp;
-                this.glpg     = glpg;
-                this.gle      = [glp , glpg];
-                if (length(this.glp) ~= length(this.glpg))
-                    error('Wrong number of pressure dofs');
-                end
-                this.nglp     = length(this.glp);
-                this.ngle     = length(this.gle);
                 this.massLumping = massLumping;
                 this.lumpStrategy = lumpStrategy;
                 this.isAxisSymmetric = isAxisSymmetric;
@@ -93,309 +82,6 @@ classdef RegularElement < handle
             end
             this.intPoint = intPts;
 
-        end
-
-        %------------------------------------------------------------------
-        % This function assembles the element matrices and vectors 
-        %
-        % Output:
-        %   Ke : element "stiffness" matrix
-        %   Ce : element "damping" matrix
-        %   fe : element "internal force" vector
-        %
-        function [Ke, Ce, fi, fe] = elementData(this)
-
-            % Initialize the sub-matrices
-            Hll = zeros(this.nglp, this.nglp);
-            Hlg = zeros(this.nglp, this.nglp);
-            Hgl = zeros(this.nglp, this.nglp);
-            Hgg = zeros(this.nglp, this.nglp);  
-            Sll = zeros(this.nglp, this.nglp);
-            Slg = zeros(this.nglp, this.nglp);
-            Sgl = zeros(this.nglp, this.nglp);
-            Sgg = zeros(this.nglp, this.nglp);
-
-            % Initialize external force vector
-            fel = zeros(this.nglp, 1);
-            feg = zeros(this.nglp, 1);
-            
-            % Vector of the nodal pore-pressure dofs
-            pc = this.getNodalCapillaryPressure();
-            pg = this.getNodalGasPressure();
-
-            % Initialize the volume of the element
-            vol = 0.0;
-
-            % Numerical integration of the sub-matrices
-            for i = 1:this.nIntPoints
-
-                % Shape function matrix
-                Np = this.shape.shapeFncMtrx(this.intPoint(i).X);
-               
-                % Compute the B matrix at the int. point and the detJ
-                [Bp, detJ] = this.shape.dNdxMatrix(this.node,this.intPoint(i).X);
-
-                % Pressure values at the integration point
-                pcIP = Np * pc;
-                pgIP = Np * pg;
-
-                % Compute the saturation degree at the integration point
-                Sl = this.intPoint(i).constitutiveMdl.saturationDegree(pcIP);
-        
-                % Compute the permeability matrix
-                [kll, klg, kgl, kgg] = this.permeabilityTensors(this.intPoint(i),pgIP,pcIP,Sl);
-
-                % Get compressibility coefficients
-                [cll, clg, cgl, cgg] = this.compressibilityCoeffs(this.intPoint(i),pgIP,pcIP,Sl);
-        
-                % Numerical integration coefficient
-                c = this.intPoint(i).w * detJ * this.t;
-                if this.isAxisSymmetric
-                    c = c * this.shape.axisSymmetricFactor(Np,this.node);
-                end
-        
-                % Compute permeability sub-matrices
-                Hll = Hll + Bp' * kll * Bp * c;
-                Hlg = Hlg + Bp' * klg * Bp * c;
-                Hgl = Hgl + Bp' * kgl * Bp * c;
-                Hgg = Hgg + Bp' * kgg * Bp * c;
-
-                % Compute compressibility matrices
-                if ((this.massLumping) && (this.lumpStrategy == 1))
-                    Sll = Sll + diag(cll*Np*c);
-                    Sgg = Sgg + diag(cgg*Np*c);
-                    Slg = Slg + diag(clg*Np*c);
-                    Sgl = Sgl + diag(cgl*Np*c);
-                elseif (this.massLumping == false)
-                    Sll = Sll + Np' * cll * Np * c;
-                    Sgg = Sgg + Np' * cgg * Np * c;
-                    Slg = Slg + Np' * clg * Np * c;
-                    Sgl = Sgl + Np' * cgl * Np * c;
-                end
-                
-                % Compute the gravity forces
-                if (this.mat.porousMedia.gravityOn)
-                    [fel,feg] = this.addGravityForces(fel,feg,Bp,kll,kgg,pgIP-pcIP,pgIP,c);
-                end
-
-                % Compute the element volume
-                vol = vol + c;
-            end
-
-            % Compute the lumped mass matrix
-            if ((this.massLumping) && (this.lumpStrategy == 2))
-                [Sll,Slg,Sgl,Sgg] = lumpedCompressibilityMatrices(this, pc, pg, vol);
-            end
-
-            % Assemble the element permeability
-            Ke = [ Hll, Hlg;
-                   Hgl, Hgg ];
-
-            % Assemble the element compressibility matrix
-            Ce = [ Sll , Slg;
-                   Sgl , Sgg ];
-
-            % Assemble element internal force vector
-            fi = Ke * this.ue;
-
-            % Assemble element external force vector
-            fe = [fel; feg];
-            
-        end
-
-        % -----------------------------------------------------------------
-        % Compute the permeability tensors
-        function [kll, klg, kgl, kgg] = permeabilityTensors(~,ip,pg,pc,Sl)
-             [kll, klg, kgl, kgg] = ip.constitutiveMdl.permeabilityMtrcs(Sl,pg-pc,pg);
-        end
-
-        % -----------------------------------------------------------------
-        % Compute the compressibility coefficients
-        function [cll, clg, cgl, cgg] = compressibilityCoeffs(~,ip,pg,pc,Sl)
-             [cll, clg, cgl, cgg] =  ip.constitutiveMdl.compressibilityCoeffs(Sl,pg-pc,pg);
-        end
-
-        %------------------------------------------------------------------
-        % Compute the lumped mass matrices
-        function [Sll,Slg,Sgl,Sgg] = lumpedCompressibilityMatrices(this, pc, pg, vol)
-
-            % Shape function matrix
-            Np = this.shape.shapeFncMtrx([0.0,0.0]);
-
-            % Pressure values at the integration point
-            pcIP = Np * pc;
-            pgIP = Np * pg;
-
-            % Compute the saturation degree at the integration point
-            Sl = this.intPoint(1).constitutiveMdl.saturationDegree(pcIP);
-
-            % Get compressibility coefficients
-            [cll, clg, cgl, cgg] = this.compressibilityCoeffs(this.intPoint(1),pgIP,pcIP,Sl);
-
-            % Mass distribution factor
-            factor = vol / this.nnd_el;
-
-            % Compressibility matrices
-            Sll = cll * factor * eye(this.nglp,this.nglp);
-            Slg = clg * factor * eye(this.nglp,this.nglp);
-            Sgl = cgl * factor * eye(this.nglp,this.nglp);
-            Sgg = cgg * factor * eye(this.nglp,this.nglp);
-
-        end
-
-        %------------------------------------------------------------------
-        % Add contribution of the gravity forces to the external force vct
-        function [fel,feg] = addGravityForces(this,fel,feg,Bp,kl,kg,pl,pg,c)
-
-            % Get gravity vector
-            grav = this.mat.porousMedia.g * this.mat.porousMedia.b;
-
-            % Get fluid densities
-            rhol = this.mat.liquidFluid.getDensity();
-            rhog = this.mat.gasFluid.getDensity();
-
-            % Compute the contribution of the gravitational forces
-            fel = fel + Bp' * kl * rhol * grav * c;
-            feg = feg + Bp' * kg * rhog * grav * c;
-            
-        end
-
-        %------------------------------------------------------------------
-        % Function to get the nodal values of the liquid pressure
-        function pl = getNodalLiquidPressure(this)
-            pl = this.ue(1:this.nglp);
-        end
-
-        %------------------------------------------------------------------
-        % Function to get the nodal values of the gas pressure
-        function pg = getNodalGasPressure(this)
-            pg = this.ue(1+this.nglp:end);
-        end
-
-        %------------------------------------------------------------------
-        % Function to get the nodal values of the capillary pressure
-        function pc = getNodalCapillaryPressure(this)
-            pl = this.getNodalLiquidPressure();
-            pg = this.getNodalGasPressure();
-            pc = pg - pl;
-        end
-
-        %------------------------------------------------------------------
-        % Function to compute the pressure field inside a given element
-        function p = pressureField(this,X,ue)
-        %
-        % Input:
-        %   X   : position vector in the global cartesian coordinate system
-        %
-        % Output:
-        %   p   : pressure evaluated in "X"
-            if nargin > 2, this.ue = ue; end
-        
-            % Natural coordinate system
-            Xn = this.shape.coordCartesianToNatural(this.node,X);
-            
-            % Vector with the shape functions
-            Nm = this.shape.shapeFncMtrx(Xn);
-
-            % Get nodal pressures
-            pl = this.getNodalLiquidPressure();
-
-            % capillary field
-            p = Nm*pl;
-        
-        end
-
-        %------------------------------------------------------------------
-        % Function to compute the pressure field inside a given element
-        function p = gasPressureField(this,X,ue)
-        %
-        % Input:
-        %   X   : position vector in the global cartesian coordinate system
-        %
-        % Output:
-        %   p   : pressure evaluated in "X"
-            if nargin > 2, this.ue = ue; end
-        
-            % Natural coordinate system
-            Xn = this.shape.coordCartesianToNatural(this.node,X);
-            
-            % Vector with the shape functions
-            Nm = this.shape.shapeFncMtrx(Xn);
-
-            % Get nodal pressures
-            pg = this.getNodalGasPressure();
-
-            % capillary field
-            p = Nm*pg;
-        
-        end
-
-        %------------------------------------------------------------------
-        % Function to compute the pressure field inside a given element
-        function p = capillaryPressureField(this,X,ue)
-        %
-        % Input:
-        %   X   : position vector in the global cartesian coordinate system
-        %
-        % Output:
-        %   p   : pressure evaluated in "X"
-            if nargin > 2, this.ue = ue; end
-        
-            % Natural coordinate system
-            Xn = this.shape.coordCartesianToNatural(this.node,X);
-            
-            % Vector with the shape functions
-            Nm = this.shape.shapeFncMtrx(Xn);
-
-            % Get nodal pressures
-            pc = this.getNodalCapillaryPressure();
-
-            % capillary field
-            p = Nm*pc;
-        
-        end
-
-        %------------------------------------------------------------------
-        % Function to compute the liquid saturation field inside a given element
-        function Sl = liquidSaturationField(this,X,ue)
-
-            if nargin > 2, this.ue = ue; end
-        
-            % Natural coordinate system
-            Xn = this.shape.coordCartesianToNatural(this.node,X);
-            
-            % Vector with the shape functions
-            Nm = this.shape.shapeFncMtrx(Xn);
-
-            % Capillary pressure at the given point
-            pc = Nm*this.getNodalCapillaryPressure();
-
-            % Compute the liquid saturation degree
-            Sl = this.intPoint(1).constitutiveMdl.saturationDegree(pc);
-        
-        end
-
-        %------------------------------------------------------------------
-        % Function to compute the gas saturation field inside a given element
-        function Sg = gasSaturationField(this,X,ue)
-            
-            if nargin > 2, this.ue = ue; end
-        
-            % Natural coordinate system
-            Xn = this.shape.coordCartesianToNatural(this.node,X);
-            
-            % Vector with the shape functions
-            Nm = this.shape.shapeFncMtrx(Xn);
-
-            % Capillary pressure at the given point
-            pc = Nm*this.getNodalCapillaryPressure();
-
-            % Compute the liquid saturation degree
-            Sl = this.intPoint(1).constitutiveMdl.saturationDegree(pc);
-
-            % Gas saturation degree
-            Sg = 1.0 - Sl;
-        
         end
 
         % -----------------------------------------------------------------
