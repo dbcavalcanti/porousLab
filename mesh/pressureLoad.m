fbc4bbe6a6a24fb709473efb565eb53a1b2b6bb2
Danilo Cavalcanti <danilocavalcanti@aluno.puc-rio.br>
2025-01-31 15:22:19 +0100

    pressure load with quadratic meshes

--- mesh/pressureLoad.m
+++ mesh/pressureLoad.m
@@ -18,17 +18,25 @@ function [LOAD] = pressureLoad(p,Xref,dir,NODE,ELEM,LOAD)
 % Number of elements
 nElem = size(ELEM,1);
 
+% Number of linear nodes of each element
+nLinNodes = size(ELEM,2); quadMesh = false;
+if (nLinNodes == 6) || (nLinNodes == 8)
+    nLinNodes = nLinNodes / 2.0;
+    quadMesh = true;
+end
+
+
 for el = 1:nElem 
 
     % Get the number of edges of the element
-    nEdges = size(ELEM,2);
+    nEdges = nLinNodes;
 
     % Get the coordinates of the element
-    cX = [NODE(ELEM(el,:),1); NODE(ELEM(el,1),1)];
-    cY = [NODE(ELEM(el,:),2); NODE(ELEM(el,1),2)];
+    cX = [NODE(ELEM(el,1:nLinNodes),1); NODE(ELEM(el,1),1)];
+    cY = [NODE(ELEM(el,1:nLinNodes),2); NODE(ELEM(el,1),2)];
 
     % Get the nodes of the borders
-    NdBorders = [ELEM(el,:), ELEM(el,1)];
+    NdBorders = [ELEM(el,1:nLinNodes), ELEM(el,1)];
 
     % Loop through the edges of the element ---------------------------
     for j = 1:nEdges
@@ -52,11 +60,17 @@ for el = 1:nElem
             dy = edgeY(2) - edgeY(1);
             l = sqrt(dx*dx + dy*dy);
 
-            % Equivalent nodal load
-            feq = 0.5*p*l;
-
             % id of the nodes of the edge
             idNds = [NdBorders(j); NdBorders(j+1)];
+
+            % Equivalent nodal load
+            if quadMesh == false
+                feq = [0.5*p*l;0.5*p*l];
+            else
+                feq = [p*l;p*l;4.0*p*l]/6.0;
+                idNds = [idNds; ELEM(el,j+nLinNodes)];
+            end
+
 
             % Add contribution to the LOAD matrix
             LOAD(idNds,dir) = LOAD(idNds,dir) + feq;
