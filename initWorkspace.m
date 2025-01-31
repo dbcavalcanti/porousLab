ae223432d373f9a5d39d77b16824546405dcad56
Danilo Cavalcanti <danilocavalcanti@aluno.puc-rio.br>
2025-01-31 14:04:24 +0100

    update header

--- initWorkspace.m
+++ initWorkspace.m
@@ -19,12 +19,15 @@ function initWorkspace()
     % Clear the workspace and the command window
     clear; clc;
     % porousLab header
-    fprintf('**********************************************************\n');
-    fprintf('*                        porousLab                       *\n');
-    fprintf('*                                                        *\n');
-    fprintf('* Created by: Danilo Cavalcanti                          *\n');
-    fprintf('* Last update: Jan/2025                                  *\n');
-    fprintf('**********************************************************\n\n');
+    fprintf('===========================================================\n');
+    fprintf('                        porousLab                          \n\n');
+    fprintf('     FEM solver for multiphysics problems in porous media\n\n');
+    fprintf('                        - CIMNE -                          \n');
+    fprintf('  International Centre for Numerical Methods in Enginnering\n');
+    fprintf('  Applied Computational Physics Group\n\n');
+    fprintf('  Author: Danilo Cavalcanti\n');
+    fprintf('  Last update: Jan/2025\n');
+    fprintf('===========================================================\n\n');
     %Use all folders and subfolders
     addpath(genpath('./'));
 end
