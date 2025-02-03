1f84ce1ff6d723e559b6496517c38a1ff1fe54d5
Danilo Cavalcanti <danilocavalcanti@aluno.puc-rio.br>
2025-02-03 17:05:51 +0100

    testing the creating of two discontinuities

--- Example_PlateDiscontinuity.m
+++ Example_PlateDiscontinuity.m
@@ -1,6 +1,6 @@
-%% ===================== Elastic plate problem ============================
+%% ========================================================================
 %
-% Elastic traction of a elastic plate validation problem
+% Block crossed by a strong discontinuity
 %
 % Author: Danilo Cavalcanti
 %
@@ -19,30 +19,34 @@ mdl = Model_M();
 % Mesh properties
 Lx = 2.0;     % Horizontal dimension (m)
 Ly = 2.0;     % Vertical dimension (m)
-Nx = 20;       % Number of elements in the x-direction
-Ny = 20;       % Number of elements in the y-direction
+Nx = 2;       % Number of elements in the x-direction
+Ny = 2;       % Number of elements in the y-direction
 
 % Generate the mesh
 [mdl.NODE,mdl.ELEM] = regularMeshY(Lx, Ly, Nx, Ny);
 
 mdl.type = 'ISOQ4';
 
-xd = [0.0, 2.0];
-yd = [0.5, 1.5];
+
 
 % xd = linspace(0, 2, 100);
 % yd = 1.0 + 0.5 * sin(0.5 * pi * xd);
-
+xd = [0.0, 2.0];
+yd = [0.5, 1.5];
 fracture = Discontinuity([xd', yd'],true);
-
-fracture.setRepelTol(0.1);
+fracture.setRepelTol(0.01);
 fracture.setSavePerturbNodes(true);
-
-% Perform intersection and repel process
 fracture.intersectMesh(mdl);
 
+xd = [0.0, 2.0];
+yd = [1.2, 0.5];
+fracture2 = Discontinuity([xd', yd'],true);
+
+% Perform intersection and repel process
+fracture2.intersectMesh(mdl);
+
 % Add the fracture to the model
-mdl.addPreExistingDiscontinuities(fracture);
+mdl.addPreExistingDiscontinuities([fracture;fracture2]);
 
 %% ============================= MATERIAL =================================
 
@@ -82,28 +86,34 @@ mdl.intOrder = 2;
 % definition, etc.)
 mdl.preComputations();
 
-% Create the result object for the analysis
-ndPlot  = 3;
-dofPlot = 1; % 1 for X and 2 for Y
-result  = ResultAnalysis(mdl.ID(ndPlot,dofPlot),[],[],[]);
-
-%% ========================== RUN ANALYSIS ================================
-
-% Solve the problem
-anl = Anl_Linear(result);
-anl.process(mdl);
-
-%% ========================= CHECK THE RESULTS ============================
-
-% Plot pressure along a segment
-Xi  = [0.0 , 0.0];
-Xf  = [0.0 , Ly];
-npts = 500;
-mdl.plotDeformedMesh(1.0);
-mdl.plotField('Ux');
-mdl.plotField('Sx');
-
-% mdl.plotField('Model'); hold on
+mdl.plotField('Model'); hold on
 % fracture.plotOriginalGeometry()
 % fracture.plotIntersectedGeometry()
+fracture2.plotIntersectedGeometry()
 % fracture.plotPerturbNodes()
+
+% Create the result object for the analysis
+% ndPlot  = 3;
+% dofPlot = 1; % 1 for X and 2 for Y
+% result  = ResultAnalysis(mdl.ID(ndPlot,dofPlot),[],[],[]);
+% 
+% %% ========================== RUN ANALYSIS ================================
+% 
+% % Solve the problem
+% anl = Anl_Linear(result);
+% anl.process(mdl);
+% 
+% %% ========================= CHECK THE RESULTS ============================
+% 
+% % Plot pressure along a segment
+% Xi  = [0.0 , 0.0];
+% Xf  = [0.0 , Ly];
+% npts = 500;
+% mdl.plotDeformedMesh(1.0);
+% mdl.plotField('Ux');
+% mdl.plotField('Sx');
+% 
+% % mdl.plotField('Model'); hold on
+% % fracture.plotOriginalGeometry()
+% % fracture.plotIntersectedGeometry()
+% % fracture.plotPerturbNodes()
