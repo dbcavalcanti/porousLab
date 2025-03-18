2e617dddd4f9bd9c756bfa6421cdeb9c18d1279a
xaviertort <xavier.tort.farres@upc.edu>
2025-03-18 15:00:45 +0100

    Update Liakopoulos test

--- Example_LiakopoulosH2M.m
+++ Example_LiakopoulosH2M.m
@@ -37,8 +37,8 @@ for i=1:size(quadratic, 2)
     % Mesh properties
     Lx = 0.1;     % Horizontal dimension (m)
     Ly = 1.0;     % Vertical dimension (m)
-    Nx = 1;       % Number of elements in the x-direction
-    Ny = 1;       % Number of elements in the y-direction
+    Nx = 3;       % Number of elements in the x-direction
+    Ny = 24;       % Number of elements in the y-direction
 
     % Generate the mesh
     [mdl.NODE,mdl.ELEM] = regularMeshY(Lx, Ly, Nx, Ny);
@@ -55,7 +55,8 @@ for i=1:size(quadratic, 2)
 
     % Create the fluids
     water = Fluid('water',1000.0, 1.0e-3, 2.0e9);
-    gas   = Fluid('gas'  ,1.20  , 1.8e-5, 1.0e5);
+    % gas   = Fluid('gas'  ,1.20  , 1.8e-5, 1.0e5);
+    gas   = IdealGas('gas', 1.8e-5, 2.0e9);
 
     % Create the porous media
     rock = PorousMedia('rock',4.5e-13,0.2975,1.0,1.0e12,0.0,0.2,0.0,3.0,'Liakopoulos','BrooksCorey','Liakopoulos');
@@ -63,53 +64,6 @@ for i=1:size(quadratic, 2)
     rock.setDensity(2000.0);
     rock.setMinLiquidRelPermeability(1.0e-4);
     rock.setMinGasRelPermeability(1.0e-4);
-
-    % Set the user material capillary pressure vs. saturation law (NOT BEING USED)
-    % --------- Pc  |  Sl
-    SlPcUMAT = [
-        9938.064, 0.900000007;
-        9516.018, 0.910000009;
-        9065.393, 0.920000003;
-        8589.271, 0.929821599;
-        8052.432, 0.939999996;
-        7469.886, 0.950000006;
-        6813.948, 0.960000005;
-        6052.562, 0.97;
-        5121.663, 0.980000004;
-        4847.584, 0.982500005;
-        4549.372, 0.984999999;
-        4220.252, 0.9875;
-        3849.668, 0.989999997;
-        3419.508, 0.9925;
-        2893.579, 0.995000002;
-        2174.942, 0.997499999;
-        1000.000, 0.99962099;
-        0.000, 1.0];
-    rock.setUMATCapillaryPressureCurve(SlPcUMAT);
-
-    % Set the user material capillary pressure vs. saturation law (NOT BEING USED)
-    % -- Sl  |  klr
-    krlUMAT = [
-        0.575, 0.00000;
-        0.600, 0.12693;
-        0.625, 0.18214;
-        0.650, 0.23730;
-        0.675, 0.29242;
-        0.700, 0.34748;
-        0.725, 0.40248;
-        0.750, 0.45743;
-        0.775, 0.51231;
-        0.800, 0.56711;
-        0.825, 0.62184;
-        0.850, 0.67646;
-        0.875, 0.73098;
-        0.900, 0.78536;
-        0.925, 0.83958;
-        0.950, 0.89358;
-        0.975, 0.94723;
-        1.000, 1.00000;
-        ];
-    rock.setUMATLiquidRelPermCurve(krlUMAT);
 
     % Activate gravity
     rock.gravityOn = true;
@@ -148,13 +102,13 @@ for i=1:size(quadratic, 2)
 
     % Gas pressure boundary conditions
     CoordSupp  = [1 -1  0;
-        1  0 -1;
-        1  Lx -1;
+        % 1  0 -1;
+        % 1  Lx -1;
         1 -1 Ly];
     CoordLoad  = [];
     CoordPresc = [101325.0 -1  0;
-        101325.0  0 -1;
-        101325.0  Lx -1;
+        % 101325.0  0 -1;
+        % 101325.0  Lx -1;
         101325.0 -1 Ly];
     CoordInit  = [];
 
@@ -195,9 +149,9 @@ for i=1:size(quadratic, 2)
     % Transient analysis parameters
     tinit = 0.1;          % Initial time
     dt    = 0.1;          % Time step
-    tf    = 60.0;         % Final time
+    tf    = 120;          % Final time
     dtmax = 1.0;          % Time step
-    dtmin = 0.0001;          % Time step
+    dtmin = 0.0001;       % Time step
 
     anl = Anl_Transient(result,"Picard");
     anl.setUpTransientSolver(tinit,dt,tf,dtmax,dtmin,true);
