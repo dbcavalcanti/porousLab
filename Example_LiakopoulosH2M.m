0de592c36c8580fbfb64bb6601253310dacf9e02
xaviertort <xavier.tort.farres@upc.edu>
2025-03-18 15:45:24 +0100

    Plot results over time

--- Example_LiakopoulosH2M.m
+++ Example_LiakopoulosH2M.m
@@ -159,8 +159,8 @@ for i=1:size(quadratic, 2)
     % Create the result object for the analysis
     ndPlot  = 3;
     dofPlot = 1; % 1 for X and 2 for Y
-    % result  = ResultAnalysis(mdl.ID(ndPlot,dofPlot),[0.05, 0.5],[0.05, 0.5],[],[0.05, 0.5],[0.05, 0.5]);
-    result  = ResultAnalysis(mdl.ID(ndPlot,dofPlot),[],[],[]);
+    result  = ResultAnalysis(mdl.ID(ndPlot,dofPlot),[0.05, 0.5],[0.05, 0.5],[],[0.05, 0.5],[0.05, 0.5]);
+    % result  = ResultAnalysis(mdl.ID(ndPlot,dofPlot),[],[],[]);
 
     %% ========================== RUN ANALYSIS ================================
 
