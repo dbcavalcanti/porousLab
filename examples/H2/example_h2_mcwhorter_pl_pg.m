36961304265d8faa1a2fa9806c5aded7bbe4c33f
Danilo Cavalcanti <danilocavalcanti@aluno.puc-rio.br>
2025-07-31 14:11:32 +0200

    update mcwhorter example

--- examples/H2/example_h2_mcwhorter_pl_pg.m
+++ examples/H2/example_h2_mcwhorter_pl_pg.m
@@ -15,12 +15,6 @@
 
 % Create model
 mdl = Model_H2();
-
-% Set model options
-mdl.massLumping  = true;  % Diagonalize compressibility matrix (mass lumping)
-mdl.lumpStrategy = 2;
-mdl.intOrder     = 3;     % Integration rule order for the domain
-
 %% MESH
 
 % Create mesh
@@ -38,15 +32,16 @@ mdl.setMesh(node, elem);
 % Create fluids
 water = Fluid('water');
 gas   = Fluid('gas');
+gas.mu = 0.005;
 
 % Create porous media
 rock = PorousMedia('rock');
 rock.K                  = 1.0e-10;        % Intrinsic permeability (m2)
-rock.phi                = 0.3;            % Porosity
-rock.Slr                = 0.0;            % Residual liquid saturation
-rock.Sgr                = 0.0;            % Residual gas saturation
+rock.phi                = 0.15;           % Porosity
+rock.Slr                = 0.02;           % Residual liquid saturation
+rock.Sgr                = 0.001;          % Residual gas saturation
 rock.Pb                 = 5.0e+3;         % Gas-entry pressure
-rock.lambda             = 2.0;            % Curve-fitting parameter
+rock.lambda             = 3.0;            % Curve-fitting parameter
 rock.liqRelPermeability = 'BrooksCorey';  % Liquid relative permeability
 rock.gasRelPermeability = 'BrooksCorey';  % Gas relative permeability
 rock.capillaryPressure  = 'BrooksCorey';  % Saturation degree function
@@ -56,20 +51,33 @@ mdl.setMaterial(rock, water, gas);
 
 %% BOUNDARY AND INITIAL CONDITIONS
 
+% Brooks and Corey capillary pressure
+pc = @(Sl) (rock.Pb * ((Sl-rock.Slr)/(1.0-rock.Slr-rock.Sgr))^(-1/rock.lambda));
+
+% Initial capillary pressure
+pc_ini = pc(0.05);
+
+% Capillary pressure at the left border
+pc_left = pc(0.8);
+
+% Initial gas pressure
+pg_ini = 100000.0;
+
 % Set Dirichlet boundary conditions
-mdl.setPressureDirichletBCAtBorder('left', 195.0e+3);
-mdl.setGasPressureDirichletBCAtBorder('left', 200.0e+3);
+mdl.setPressureDirichletBCAtBorder('left', pg_ini - pc_left);
+mdl.setGasPressureDirichletBCAtBorder('left', pg_ini);
 
 % Set initial conditions
-mdl.setInitialPressureAtDomain(-50.0e+3);
+mdl.setInitialPressureAtDomain(pg_ini - pc_ini);
+mdl.setInitialGasPressureAtDomain(pg_ini);
 
 %% PROCESS
 
 % Analysis parameters
-ti        = 0.001;        % Initial time
-dt        = 0.001;        % Time step
+ti        = 0.001;      % Initial time
+dt        = 0.001;      % Time step
 tf        = 1000;       % Final time
-dtmax     = 10.0;       % Maximum time step
+dtmax     = 5.0;        % Maximum time step
 dtmin     = 0.0000001;  % Minimum time step
 adaptStep = true;       % Adaptive step size
 
@@ -86,4 +94,4 @@ mdl.plotField('CapillaryPressure');
 
 % Plot graphs
 Xi = [0.0, 0.0]; Xf = [Lx, 0.0];
-mdl.plotFieldAlongSegment('LiquidPressure', Xi, Xf, 500, 'x');
+mdl.plotFieldAlongSegment('LiquidSaturation', Xi, Xf, 500, 'x');
